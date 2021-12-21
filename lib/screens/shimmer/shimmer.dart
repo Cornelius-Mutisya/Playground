66b4002353137f6c5c6aee3b533bd42094cd7f6c
corneliusmutisya11@gmail <corneliusmutisya11@gmail.com>
2021-12-21 23:02:43 +0300

    Firebase setup

--- lib/screens/shimmer/shimmer.dart
+++ lib/screens/shimmer/shimmer.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:playground/size_config.dart';
 import 'package:shimmer/shimmer.dart';
 
 class ShimmerScreen extends StatefulWidget {
@@ -11,11 +12,12 @@ class ShimmerScreen extends StatefulWidget {
 }
 
 class _ShimmerScreenState extends State<ShimmerScreen> {
+  bool _enabled = true;
   @override
   void initState() {
-    Timer(const Duration(seconds: 5), () {
-      Navigator.pop(context);
-    });
+    // Timer(const Duration(seconds: 5), () {
+    //   Navigator.pop(context);
+    // });
     super.initState();
   }
 
@@ -26,25 +28,86 @@ class _ShimmerScreenState extends State<ShimmerScreen> {
         title: const Text('Shimmer'),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: 100.0,
-              child: Shimmer.fromColors(
-                baseColor: Colors.red,
-                highlightColor: Colors.yellow,
-                child: const Text(
-                  'Shimmer for 5 seconds',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(16),
+            vertical: getProportionateScreenHeight(16),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Expanded(
+                child: Shimmer.fromColors(
+                  baseColor: Colors.grey.shade300,
+                  highlightColor: Colors.grey.shade100,
+                  enabled: _enabled,
+                  child: ListView.builder(
+                    itemBuilder: (_, __) => Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            width: 48.0,
+                            height: 48.0,
+                            color: Colors.white,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  width: double.infinity,
+                                  height: 8.0,
+                                  color: Colors.white,
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 2.0),
+                                ),
+                                Container(
+                                  width: double.infinity,
+                                  height: 8.0,
+                                  color: Colors.white,
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 2.0),
+                                ),
+                                Container(
+                                  width: 40.0,
+                                  height: 8.0,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    itemCount: 6,
                   ),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _enabled = !_enabled;
+                      });
+                    },
+                    child: Text(
+                      _enabled ? 'Stop' : 'Play',
+                      style: Theme.of(context).textTheme.button!.copyWith(
+                          fontSize: 18.0,
+                          color: _enabled ? Colors.redAccent : Colors.green),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
